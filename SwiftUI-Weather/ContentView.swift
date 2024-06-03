673c75687d071635bac58c7c54db7f1c31352b5a
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-03 23:37:34 +0530

    Implement ZStack and city name.

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //to set background gradient (color)
+            //in below example color will strt with blue and wll go to white, these colors to be specified in an array, multiple colors can be used (not just 2)
+            //startpoint is from top left (where 1st color willb used), endpt is at bottom-right corner (to end with white)
+            //edgesIgnoringSafeArea property used to cover all area and not just safe area of the screen
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), 
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                //each modifier (.property) wraps this view into another view
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
+            
         }
-        .padding()
     }
 }
 
